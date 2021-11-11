088405a0f02e61b7d69335e663e085fb844bbef0
HerculeGuan <15221038479@163.com>
2021-11-11 23:00:07 +0800

    添加food类

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 import "./style/index.less"
+class Food {
+    element: HTMLElement;
+    constructor() {
+        this.element = document.getElementById('food')!;
+    }
+    get x() {
+        return this.element.offsetLeft
+    }
+    get y() {
+        return this.element.offsetTop
+    }
+    changePosition() {
+        let randomLeft = Math.round(Math.random() * 29) * 10
+        let randomRight = Math.round(Math.random() * 29) * 10
+        this.element.style.left = `${randomLeft}px`
+        this.element.style.top = `${randomRight}px`
+    }
+}
+const food = new Food();
+setInterval(() => {
+    food.changePosition()
+}, 1000)
